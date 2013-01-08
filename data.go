1ad68b3e8ebf86e6bc3c0b3cb9a2926d5ba2e333
Dustin Sallings <dustin@spy.net>
2013-01-08 13:45:33 -0800

    Saving and showing bugs.

---
+++ data.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+)
+
+type Bug struct {
+	Id          string    `json:"-"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	Creator     string    `json:"creator"`
+	Tags        []string  `json:"tags"`
+	Type        string    `json:"type"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func (b Bug) Url() string {
+	return "/bug/" + b.Id
+}
--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/couchbaselabs/go-couchbase"
 )
 
 var templates *template.Template
+var db *couchbase.Bucket
 
 var staticPath = flag.String("static", "static", "Path to the static content")
 
@@ -19,8 +23,80 @@ func init() {
 		panic("Couldn't parse templates.")
 	}
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/bug/", serveBugPath)
 	http.Handle("/static/", http.StripPrefix("/static/",
 		http.FileServer(http.Dir(*staticPath))))
+}
+
+func newBugId() (uint64, error) {
+	return db.Incr(".bugid", 1, 0, 0)
+}
+
+func showError(w http.ResponseWriter, r *http.Request,
+	msg string, code int) {
+	http.Error(w, msg, code)
+}
+
+func serveNewBug(w http.ResponseWriter, r *http.Request) {
+	id, err := newBugId()
+	if err != nil {
+		showError(w, r, err.Error(), 500)
+		return
+	}
+
+	bug := Bug{
+		Id:          fmt.Sprintf("bug-%v", id),
+		Title:       r.FormValue("title"),
+		Description: r.FormValue("description"),
+		Status:      "new",
+		Creator:     "me", // XXX: Better
+		Tags:        r.Form["tag"],
+		Type:        "bug",
+		CreatedAt:   time.Now().UTC(),
+	}
+
+	added, err := db.Add(bug.Id, 0, bug)
+	if err != nil {
+		showError(w, r, err.Error(), 500)
+		return
+	}
+	if !added {
+		// This is a bug bug
+		showError(w, r, "Bug collision on "+bug.Id, 500)
+		return
+	}
+
+	http.Redirect(w, r, bug.Url(), 303)
+}
+
+func serveBug(w http.ResponseWriter, r *http.Request, id string) {
+	bug := Bug{}
+	err := db.Get(id, &bug)
+	if err != nil {
+		showError(w, r, err.Error(), 404)
+		return
+	}
+	bug.Id = id
+
+	templates.ExecuteTemplate(w, "bug.html", bug)
+}
+
+func minusPrefix(s, prefix string) string {
+	return s[len(prefix):]
+}
+
+func serveBugPath(w http.ResponseWriter, r *http.Request) {
+	path := minusPrefix(r.URL.Path, "/bug/")
+
+	switch {
+	case r.Method == "POST" && path == "":
+		serveNewBug(w, r)
+	case r.Method == "GET":
+		serveBug(w, r, path)
+	default:
+		showError(w, r, "Can't service "+r.Method+":"+path,
+			http.StatusMethodNotAllowed)
+	}
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +104,23 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		map[string]interface{}{})
 }
 
+func initCb(serv, bucket string) {
+	var err error
+	db, err = couchbase.GetBucket(serv, "default", bucket)
+	if err != nil {
+		log.Fatalf("Error connecting to couchbase: %v", err)
+	}
+}
+
 func main() {
 	addr := flag.String("addr", ":8066", "http listen address")
+	cbServ := flag.String("couchbase", "http://localhost:8091/",
+		"URL to couchbase")
+	cbBucket := flag.String("bucket", "cbugg", "couchbase bucket")
 
 	flag.Parse()
+
+	initCb(*cbServ, *cbBucket)
 
 	log.Printf("Listening on %v", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
