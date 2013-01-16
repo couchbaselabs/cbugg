b18dcacfa21b13f7fdfe04aed09902cd5d7986ce
Dustin Sallings <dustin@spy.net>
2013-01-15 20:39:06 -0800

    Tag links.

--- static/cbugg.js
+++ static/cbugg.js
@@ -326,6 +326,12 @@ function BugCtrl($scope, $routeParams, $http, $rootScope) {
         $scope.editingowner = true;
     }
 
+    $scope.listTag = function(tagname) {
+        var url = '/search/tags:%22' + encodeURIComponent(tagname) +
+            '%22%20AND%20(status:open%20OR%20status:new)';
+        $location.path("/search/" + $scope.query);
+    }
+
     $scope.submitOwner = function() {
         $scope.bug.owner.email = $(".ownerbox").val();
         updateBug("owner", $scope.bug.owner.email);
