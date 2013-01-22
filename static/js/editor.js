8db8966493bf52c6220051e3d2704a702f875651
Aaron Miller <apage43@ninjawhale.com>
2013-01-21 17:28:48 -0800

    Move editor and bug page out of cbugg.js

---
+++ static/js/editor.js
@@ -0,0 +1,57 @@
+angular.module('cbuggEditor', [])
+    .directive('cbMirror', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, el, attrs, ngModel) {
+                scope.setupMirror = function() {
+                    scope.codeMirror = CodeMirror.fromTextArea(el[0], {
+                        theme: 'cb',
+                        mode: {name: 'gfm'},
+                        lineWrapping: true
+                    });
+                    if(scope.live) {
+                        scope.codeMirror.on("change", function(cm, change) {
+                            var val = cm.getValue();
+                            if(val !== ngModel.$viewValue) {
+                                ngModel.$setViewValue(val);
+                                scope.$apply();
+                            }
+                        });
+                    }
+                    ngModel.$render = function() {
+                        scope.codeMirror.setValue(ngModel.$viewValue);
+                    }
+                }
+                $timeout(scope.setupMirror);
+            }
+        }
+    }])
+    .directive('cbEditor', function () {
+        var editortpl = '<div ng-class="{edithide: !editing}"><textarea ng-model="source" '+
+                        'cb-mirror></textarea>Format with <a href="http://daringfireball.net'+
+                        '/projects/markdown/syntax">Markdown</a></div>';
+        return {
+            restrict: 'E',
+            scope: {
+                source: '=',
+                editing: '=',
+                editfn: '=',
+                live: '@'
+            },
+            compile: function(tElem, tAttrs) {
+                tElem.prepend(editortpl);
+                return {
+                    post: function(scope, el, attrs) {
+                        scope.editfn = function() {
+                            scope.editing = true;
+                        }
+                        scope.save = function() {
+                            scope.editing = false;
+                            scope.source = scope.codeMirror.getValue();
+                        }
+                    }
+                };
+            }
+        };
+    });
