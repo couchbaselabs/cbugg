8d7549316cdb3bb6cb9f13058249949023f766a0
Marty Schoch <marty.schoch@gmail.com>
2013-01-22 15:44:52 -0800

    refactored search into more composable functions

--- search.go
+++ search.go
@@ -12,6 +12,15 @@ import (
 	"github.com/mattbaird/elastigo/core"
 )
 
+// by default we only want documents of elasticsearch type "couchbaseDocument"
+// and documents with type "bug"
+func getDefaultFilterComponents() []Filter {
+	return []Filter{
+		buildTypeFilter("couchbaseDocument"),
+		buildTermFilter("doc.type", "bug"),
+	}
+}
+
 func searchBugs(w http.ResponseWriter, r *http.Request) {
 
 	api.Domain = *esHost
@@ -28,69 +37,31 @@ func searchBugs(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	statsFilter := map[string]interface{}{}
-	if r.FormValue("status") != "" {
-		statsFilter = map[string]interface{}{
-			"terms": map[string]interface{}{
-				"doc.status": strings.Split(r.FormValue("status"), ","),
-			},
-		}
-	}
-
-	tagsFilter := map[string]interface{}{}
-	if r.FormValue("tags") != "" {
-		statsFilter = map[string]interface{}{
-			"terms": map[string]interface{}{
-				"doc.tags":  strings.Split(r.FormValue("tags"), ","),
-				"execution": "and",
-			},
-		}
-	}
-
 	if r.FormValue("query") != "" {
 
-		filter := map[string]interface{}{
-			"and": []interface{}{
-				map[string]interface{}{
-					"type": map[string]interface{}{
-						"value": "couchbaseDocument",
-					},
-				},
-				map[string]interface{}{
-					"term": map[string]interface{}{
-						"doc.type": "bug",
-					},
-				},
-				statsFilter,
-				tagsFilter,
-			},
+		filterComponents := getDefaultFilterComponents()
+
+		if r.FormValue("status") != "" {
+			statusFilter := buildTermsFilter("doc.status", strings.Split(r.FormValue("status"), ","), "")
+			filterComponents = append(filterComponents, statusFilter)
 		}
 
-		query := map[string]interface{}{
-			"query": map[string]interface{}{
-				"query_string": map[string]interface{}{
-					"query": r.FormValue("query"),
-				},
-			},
-			"filter": filter,
-			"from":   from,
-			"facets": map[string]interface{}{
-				"statuses": map[string]interface{}{
-					"terms": map[string]interface{}{
-						"field": "doc.status",
-						"size":  5,
-					},
-					"facet_filter": filter,
-				},
-				"tags": map[string]interface{}{
-					"terms": map[string]interface{}{
-						"field": "doc.tags",
-						"size":  5,
-					},
-					"facet_filter": filter,
-				},
-			},
+		if r.FormValue("tags") != "" {
+			tagsFilter := buildTermsFilter("doc.tags", strings.Split(r.FormValue("tags"), ","), "and")
+			filterComponents = append(filterComponents, tagsFilter)
 		}
+
+		filter := buildAndFilter(filterComponents)
+
+		statusFacet := buildTermsFacet("doc.status", filter, 5)
+		tagsFacet := buildTermsFacet("doc.tags", filter, 5)
+
+		facets := Facets{
+			"statuses": statusFacet,
+			"tags":     tagsFacet,
+		}
+
+		query := buildQueryStringQuery(r.FormValue("query"), filter, facets, from)
 
 		searchresponse, err := core.Search(false, *esIndex, "couchbaseDocument", query, "")
 		if err != nil {
@@ -98,65 +69,7 @@ func searchBugs(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		ourresponse := map[string]interface{}{
-			"took":      searchresponse.Took,
-			"timed_out": searchresponse.TimedOut,
-			"hits": map[string]interface{}{
-				"total": 0,
-				"hits":  []interface{}{},
-			},
-			"_shards":    searchresponse.ShardStatus,
-			"_scroll_id": searchresponse.ScrollId,
-		}
-
-		if searchresponse.Hits.Total > 0 {
-			hitrecords := make([]interface{}, 0)
-
-			ids := make([]string, 0)
-
-			// walk through the hits, building list of ids
-			for _, hit := range searchresponse.Hits.Hits {
-				ids = append(ids, hit.Id)
-			}
-
-			// bulk get the docs we're interested in
-			bulkResponse := db.GetBulk(ids)
-
-			// walk through the hits again, adding the original document to the source
-			for _, hit := range searchresponse.Hits.Hits {
-
-				// find the couchbase response for this hit
-				mcResponse := bulkResponse[hit.Id]
-				cbDoc := map[string]interface{}{}
-				// unmarshall the json
-				jsonErr := json.Unmarshal(mcResponse.Body, &cbDoc)
-				if jsonErr != nil {
-					// if any error occurred, assume it wasnt json, return full data as base64
-					cbDoc = map[string]interface{}{"base64": base64.StdEncoding.EncodeToString(mcResponse.Body)}
-				}
-
-				ourhit, err := combineSearchHitWithDoc(hit, cbDoc)
-				if err != nil {
-					log.Printf("%v", err)
-					continue
-				}
-				hitrecords = append(hitrecords, ourhit)
-			}
-
-			ourfacets := map[string]interface{}{}
-			err = json.Unmarshal(searchresponse.Facets, &ourfacets)
-			if err != nil {
-				log.Printf("Error unmarshalling search result facets: %v", err)
-			}
-
-			ourhits := map[string]interface{}{
-				"total": searchresponse.Hits.Total,
-				"hits":  hitrecords,
-			}
-			ourresponse["hits"] = ourhits
-			ourresponse["facets"] = ourfacets
-
-		}
+		ourresponse := convertElasticSearchResponse(searchresponse)
 
 		jres, err := json.Marshal(ourresponse)
 		if err != nil {
@@ -170,6 +83,79 @@ func searchBugs(w http.ResponseWriter, r *http.Request) {
 	showError(w, r, "Search query cannot be empty", 500)
 }
 
+// this unfortunate function converse a response from Elasticsearch
+// into one we can return to the caller
+// primarily it decodes some fields into JSON which were left as RawMessage
+// by the elasticsearch library we're using
+func convertElasticSearchResponse(searchresponse core.SearchResult) map[string]interface{} {
+	ourresponse := map[string]interface{}{
+		"took":      searchresponse.Took,
+		"timed_out": searchresponse.TimedOut,
+		"hits": map[string]interface{}{
+			"total": 0,
+			"hits":  []interface{}{},
+		},
+		"_shards":    searchresponse.ShardStatus,
+		"_scroll_id": searchresponse.ScrollId,
+	}
+
+	if searchresponse.Hits.Total > 0 {
+		hitrecords := make([]interface{}, 0)
+
+		ids := make([]string, 0)
+
+		// walk through the hits, building list of ids
+		for _, hit := range searchresponse.Hits.Hits {
+			ids = append(ids, hit.Id)
+		}
+
+		// bulk get the docs we're interested in
+		bulkResponse := db.GetBulk(ids)
+
+		// walk through the hits again, adding the original document to the source
+		for _, hit := range searchresponse.Hits.Hits {
+
+			// find the couchbase response for this hit
+			mcResponse := bulkResponse[hit.Id]
+			cbDoc := map[string]interface{}{}
+			// unmarshall the json
+			jsonErr := json.Unmarshal(mcResponse.Body, &cbDoc)
+			if jsonErr != nil {
+				// if any error occurred, assume it wasnt json, return full data as base64
+				cbDoc = map[string]interface{}{"base64": base64.StdEncoding.EncodeToString(mcResponse.Body)}
+			}
+
+			ourhit, err := combineSearchHitWithDoc(hit, cbDoc)
+			if err != nil {
+				log.Printf("%v", err)
+				continue
+			}
+			hitrecords = append(hitrecords, ourhit)
+		}
+
+		ourfacets := map[string]interface{}{}
+		err := json.Unmarshal(searchresponse.Facets, &ourfacets)
+		if err != nil {
+			log.Printf("Error unmarshalling search result facets: %v", err)
+		}
+
+		ourhits := map[string]interface{}{
+			"total": searchresponse.Hits.Total,
+			"hits":  hitrecords,
+		}
+		ourresponse["hits"] = ourhits
+		ourresponse["facets"] = ourfacets
+
+	}
+
+	return ourresponse
+}
+
+// this function takes a document from couchbase
+// and merges it into a search result hit
+// resulting in a response that looks like it contained a complete document
+// even though ElasticSearch only had the meta data
+// and we looked up the full document bodies in Couchbase
 func combineSearchHitWithDoc(hit core.Hit, doc interface{}) (map[string]interface{}, error) {
 	result := map[string]interface{}{
 		"_index": hit.Index,
@@ -189,4 +175,71 @@ func combineSearchHitWithDoc(hit core.Hit, doc interface{}) (map[string]interfac
 	result["source"] = source
 
 	return result, nil
+}
+
+// ---------------------------------------
+// Elasticsearch utility functions
+// ---------------------------------------
+
+type Filter map[string]interface{}
+type Query map[string]interface{}
+type Facet map[string]interface{}
+type Facets map[string]interface{}
+
+func buildTermsFilter(field string, terms []string, execution string) Filter {
+	if execution == "" {
+		execution = "bool"
+	}
+
+	return Filter{
+		"terms": map[string]interface{}{
+			field:       terms,
+			"execution": execution,
+		},
+	}
+}
+
+func buildTermFilter(field string, term string) Filter {
+	return Filter{
+		"term": map[string]interface{}{
+			field: term,
+		},
+	}
+}
+
+func buildTypeFilter(typ string) Filter {
+	return Filter{
+		"type": map[string]interface{}{
+			"value": typ,
+		},
+	}
+}
+
+func buildAndFilter(components []Filter) Filter {
+	return Filter{
+		"and": components,
+	}
+}
+
+func buildTermsFacet(field string, filter Filter, size int) Facet {
+	return map[string]interface{}{
+		"terms": map[string]interface{}{
+			"field": field,
+			"size":  size,
+		},
+		"facet_filter": filter,
+	}
+}
+
+func buildQueryStringQuery(queryString string, filter Filter, facets Facets, from int) Query {
+	return Query{
+		"query": map[string]interface{}{
+			"query_string": map[string]interface{}{
+				"query": queryString,
+			},
+		},
+		"filter": filter,
+		"from":   from,
+		"facets": facets,
+	}
 }
