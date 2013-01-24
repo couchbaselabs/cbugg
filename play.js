49a5beff173db19ffa52a2dfcea61e8403d979d1
Dustin Sallings <dustin@spy.net>
2013-01-24 00:33:11 -0800

    Trying to semi-offline a 'present' thing.

---
+++ play.js
@@ -0,0 +1,1 @@
+document.write(unescape("%3Cscript src='static%2Fsocket.js' type='text/javascript'%3E%3C/script%3E"));document.write(unescape("%3Cscript src='static%2Fplay.js' type='text/javascript'%3E%3C/script%3E"));
