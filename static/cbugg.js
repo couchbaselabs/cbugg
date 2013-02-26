01b6b87a88869a369868db7df6c6e39502d70f91
Dustin Sallings <dustin@spy.net>
2013-02-26 12:39:25 -0800

    Sort interior bug lists with most recently modified first.
    cbugg: close bug-358

--- static/cbugg.js
+++ static/cbugg.js
@@ -83,6 +83,9 @@ function bugListDataPrep(data) {
     var grouped = _.pairs(_.groupBy(data, function(e) { return e.Value.Owner.md5; }));
     var nameMap = _.object(_.map(data, function(e){return e.Value.Owner.md5;}),
                            _.map(data, function(e){return e.Value.Owner.email;}));
+    _.forEach(grouped, function(x) {
+        x[1] = _.sortBy(x[1], function(e) { return e.Value.Mod; }).reverse();
+    });
     return _.sortBy(grouped, function(n) { return nameMap[n[0]]; });
 }
 
